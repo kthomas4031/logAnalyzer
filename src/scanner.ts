9810a18efc54f77778e44c04ad96dcf6cdc48f5a
Kyle Thomas <kthomas4031@floridapoly.edu>
2018-06-28 13:59:22 -0400

    Fixed Brian's Mistakes AKA Made the remove properties function for typescript

--- src/scanner.ts
+++ src/scanner.ts
@@ -2,87 +2,99 @@ import * as fs from "fs";
 import { createInterface } from "readline";
 
 const whitelistJSON = [
-	"name",
-	"errorCode",
-	"service",
-	"level",
-	"error",
-	"err",
-	"resErr",
-	"msg",
-	"eventName",
-	"taskType",
-	"httpStatusCode",
-	"httpStatusMessage",
-	"registrationData",
-	"message",
-	"command",
-	"resStatusCode",
-	"initializingModule",
-	"relation",
-	"type",
-	"maintenanceType",
-	"processName",
-	"reason",
-	"pm2Command",
-	"resSentPart",
-	"processName",
-	"instanceName",
-	"description",
-	"serviceStatus",
-	"msoLicenseStatus",
-	"taskOutput",
-	"data",
-	"req" ,
-	"method",
-	"taskInput",
-	"callingService",
-	"parent"
+  "name",
+  "errorCode",
+  "service",
+  "level",
+  "error",
+  "err",
+  "resErr",
+  "msg",
+  "eventName",
+  "taskType",
+  "httpStatusCode",
+  "httpStatusMessage",
+  "registrationData",
+  "message",
+  "command",
+  "resStatusCode",
+  "initializingModule",
+  "relation",
+  "type",
+  "maintenanceType",
+  "processName",
+  "reason",
+  "pm2Command",
+  "resSentPart",
+  "processName",
+  "instanceName",
+  "description",
+  "serviceStatus",
+  "msoLicenseStatus",
+  "taskOutput",
+  "data",
+  "req",
+  "method",
+  "taskInput",
+  "callingService",
+  "parent"
 ];
 
+function removeProps(objectJSON) {
+  for (let key in objectJSON) {
+    if (objectJSON.hasOwnProperty(key)) {
+      if (whitelistJSON.indexOf(key) === -1 || objectJSON[key] == "") {
+        delete objectJSON[key];
+      } else if (typeof objectJSON[key] == `object`) {
+        removeProps(objectJSON[key]);
+      }
+    }
+  }
+  return objectJSON;
+}
+
 export function scanLines(sanitizedFiles) {
-	let writeStream = fs.createWriteStream("output.txt");
+  let writeStream = fs.createWriteStream("output.txt");
 
-	for (let x = 0; x < sanitizedFiles.length; x++) {
-		const readline = createInterface({
-			"input": fs.createReadStream(sanitizedFiles[x])
-		});
+  for (let x = 0; x < sanitizedFiles.length; x++) {
+    const readline = createInterface({
+      input: fs.createReadStream(sanitizedFiles[x])
+    });
+    let lineNumber = 0;
+    let uniqueJSONObjects = [];
 
-		let lines = [];
-		let lineNumber = 0;
-		let uniqueJSONObjects = [];
+    let count = {};
 
-		let count = {};
+    readline.on("line", function(line) {
+      lineNumber += 1;
+      if (line[0] === "{") {
+        let jsonObject = JSON.parse(line);
+        if (jsonObject !== undefined) {
+          removeProps(jsonObject);
 
-		readline.on("line", function(line) {
-			lineNumber += 1;
-			if (line[0] === "{") {
-				let jsonObject = JSON.parse(line);
-				if (jsonObject !== undefined) {
-					Object.keys(jsonObject).forEach(function(key) {
-						delete jsonObject[key];
-					});
+          if (count[JSON.stringify(jsonObject)] === undefined) {
+            count[JSON.stringify(jsonObject)] = 1;
+          } else {
+            count[JSON.stringify(jsonObject)] += 1;
+          }
 
-					if (count[JSON.stringify(jsonObject)] === undefined) {
-						count[JSON.stringify(jsonObject)] = 1;
-					} else {
-						count[JSON.stringify(jsonObject)] += 1;
-					}
+          uniqueJSONObjects.push(JSON.stringify(Object.keys(jsonObject)));
+        } else {
+          console.error(
+            "Error Parsing JSON line " + lineNumber + " in " + sanitizedFiles[x]
+          );
+        }
+      }
+    });
 
-					uniqueJSONObjects.push(JSON.stringify(Object.keys(jsonObject)));
-				} else {
-					console.error("Error Parsing JSON line " + lineNumber + " in " + sanitizedFiles[x]);
-				}
-			}
-		});
+    readline.on("close", function() {
+      let outputJSON = JSON.stringify(count, undefined, 4);
+      outputJSON = outputJSON.replace(/\\"/g, '"');
 
-		readline.on("close", function() {
-			count = JSON.stringify(count, undefined, 4);
-
-			writeStream.write("\n === " + sanitizedFiles[x] + " === \n" + count);
-			if ((x + 1) < sanitizedFiles.length) {
-				console.log("Starting work on file: " + sanitizedFiles[x + 1]);
-			}
-		});
-	}
+      writeStream.write("\n === " + sanitizedFiles[x] + " === \n" + outputJSON);
+      if (x + 1 < sanitizedFiles.length) {
+        console.log("Starting work on file: " + sanitizedFiles[x + 1]);
+      }
+    });
+  }
 }
